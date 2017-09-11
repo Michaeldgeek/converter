35df0aa895caf36698240021161f9b1b82709882
Michaeldgeek <petermichael908@gmail.com>
2017-09-11 14:43:30 +0100

    lol

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ app.get('/excel_to_pdf', function(req, res) {
     res.render('excel_to_pdf');
 });
 
+app.get('/pdf_to_jpg', function(req, res) {
+    res.render('pdf_to_jpg');
+});
+
 app.post('/word_to_pdf', multipartyMiddleware, function(req, res) {
     fs.readFile(req.files.file.path, function(err, data) {
         var file = req.files.file;
@@ -86,7 +90,7 @@ app.post('/excel_to_pdf', multipartyMiddleware, function(req, res) {
         file.fullPath = config.TEMP + file.name;
         if ((file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' && file.type !== 'application/vnd.ms-excel') && (file.type !== '.xlsx' && file.type !== '.xls')) {
             res.status(404);
-            res.send(file.type);
+            res.send("Select an Excel document");
             return;
         }
         fs.writeFile(file.fullPath, data, function(err) {
@@ -123,6 +127,27 @@ app.post('/jpg_to_pdf', multipartyMiddleware, function(req, res) {
     });
 });
 
+app.post('/pdf_to_jpg', multipartyMiddleware, function(req, res) {
+    fs.readFile(req.files.file.path, function(err, data) {
+        var file = req.files.file;
+        file.fullPath = config.TEMP + file.name;
+        if (file.type !== 'application/pdf') {
+            res.status(404);
+            res.send("Select a pdf document");
+            return;
+        }
+        fs.writeFile(file.fullPath, data, function(err) {
+            if (err) {
+                console.log(err);
+                res.send('error');
+                return;
+            }
+            res.send('ok');
+        });
+
+    });
+});
+
 var jsonParser = bodyParser.json()
 app.post('/convert_to_pdf', jsonParser, function(req, res) {
     var data = req.body;
@@ -135,6 +160,38 @@ app.post('/convert_to_pdf', jsonParser, function(req, res) {
         file.convertTo = element.split('.')[0].trim() + ".pdf";
         file.writeTo = config.TEMP + file.convertTo;
         unoconv.convert(file.fullPath, 'pdf', function(err, result) {
+            // result is returned as a Buffer
+            fs.writeFileSync(file.writeTo, result);
+            var output = fs.createWriteStream(__dirname + '/output.zip');
+            var archive = archiver('zip', {
+                gzip: true,
+                zlib: { level: 9 } // Sets the compression level.
+            });
+            archive.on('error', function(err) {
+                console.log(err);
+            });
+            output.on('close', function() {
+                res.download(__dirname + '/output.zip');
+            });
+            archive.pipe(output);
+            archive.append(fs.createReadStream(file.writeTo), { name: file.convertTo });
+            archive.finalize();
+        });
+    });
+
+});
+
+app.post('/convert_from_pdf', jsonParser, function(req, res) {
+    var data = req.body;
+    var convertedFiles = [];
+    data.forEach(function(element, index, array) {
+        var file = {
+
+        };
+        file.fullPath = config.TEMP + element;
+        file.convertTo = element.split('.')[0].trim() + ".pdf";
+        file.writeTo = config.TEMP + file.convertTo;
+        unoconv.convert(file.fullPath, 'jpg', function(err, result) {
             // result is returned as a Buffer
             fs.writeFileSync(file.writeTo, result);
             var output = fs.createWriteStream(__dirname + '/output.zip');
