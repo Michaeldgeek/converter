d3d8e41f5e31d7196f87d64efb9f31950178e969
Michaeldgeek <petermichael908@gmail.com>
2017-09-14 22:39:10 +0100

    lol

--- index.js
+++ index.js
@@ -253,7 +253,7 @@ app.post('/convert_from_pdf_word', jsonParser, function(req, res) {
                 console.log(err);
                 return;
             }
-            var code = shell.exec('sudo /snap/bin/libreoffice --infilter="writer_pdf_import" --convert-to doc ' + 'pdf/' + element).code;
+            var code = shell.exec('sudo /snap/bin/libreoffice --infilter="writer_pdf_import" --convert-to doc ' + +element).code;
             console.log(code);
 
         });
