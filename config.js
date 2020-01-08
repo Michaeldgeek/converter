2da3f518aa89aae4cc58b20f95899d3d08823de4
Michael <petermichael908@gmail.com>
2020-01-08 18:35:05 +0100

    changed to port 80

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     TEMP: './temp/',
     LIBRE_OFFICE_PATH: '/snap/bin/pdf/',
     LIBRE_OFFICE_OUTPUT_PATH: '/var/lib/snapd/void/',
-    PORT: '80',
+    PORT: '8080',
     DOWNLOADS: 'downloads/'
 
 };
